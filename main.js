052f636ae40098fe0c9b63ae360aae5d5bb8e0b3
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-01-15 21:15:23 -0500

    scaling

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const gridSize = document.querySelector('#gridSize');
 
 const createDiv = (numDivs) =>{
     sketchDiv.innerHTML = '';
+    const scale = 100/numDivs;
     for(let i = 0; i<numDivs; i++){
         let newDiv = document.createElement('div');
+        
         for(let j = 0; j<numDivs; j++){
             let innerDiv = document.createElement('div');
-            innerDiv.innerHTML='_';
+            innerDiv.style.padding = `${scale}px`;
+            
+            innerDiv.innerText='_';
+            
             newDiv.appendChild(innerDiv);
             
         }
