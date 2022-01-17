c03b06bec5316fb67f12adc2d6819b3292cf89db
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-01-17 15:25:41 -0500

    grid dimensions

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 const sketchDiv = document.querySelector('#sketch-container');
 const gridSize = document.querySelector('#gridSize');
+const gridVal = document.querySelector('#gridVal');
 
 const createDiv = (numDivs) =>{
+
+    gridVal.innerHTML = '';
     sketchDiv.innerHTML = '';
+
+    gridVal.innerText=`${numDivs} x ${numDivs} `;
     const scale = 100/numDivs;
+
     for(let i = 0; i<numDivs; i++){
         let newDiv = document.createElement('div');
         
         for(let j = 0; j<numDivs; j++){
             let innerDiv = document.createElement('div');
-            innerDiv.style.padding = `${scale}px`;
+            innerDiv.className = 'innerDiv';
+
+            gridFormat(innerDiv,scale);
             
-            innerDiv.innerText='_';
+            innerDiv.addEventListener('mouseover',()=>{
+                innerDiv.setAttribute('style','background-color:black;');
+                gridFormat(innerDiv,scale);
+            });
             
             newDiv.appendChild(innerDiv);
             
@@ -22,6 +33,10 @@ const createDiv = (numDivs) =>{
 
 }
 
+const gridFormat = (div,scale) =>{
+    div.style.padding = `${scale}px `;     
+    div.innerText='_';
+}
 
 
 createDiv(16);
