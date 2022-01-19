2601dbcc067dc57b786df1f1e9d09f63d33b5e4a
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-01-18 22:27:51 -0500

    fixed grid generation

--- main.js
+++ main.js
@@ -2,26 +2,34 @@ const sketchDiv = document.querySelector('#sketch-container');
 const gridSize = document.querySelector('#gridSize');
 const gridVal = document.querySelector('#gridVal');
 
+
 const createDiv = (numDivs) =>{
 
+    const containDimension = sketchDiv.clientWidth;
     gridVal.innerHTML = '';
     sketchDiv.innerHTML = '';
 
     gridVal.innerText=`${numDivs} x ${numDivs} `;
-    const scale = 100/numDivs;
+
 
     for(let i = 0; i<numDivs; i++){
         let newDiv = document.createElement('div');
+        
+
+        newDiv.style.width=`${containDimension}px`;
+        newDiv.style.height=`${containDimension/numDivs}px`;
         
         for(let j = 0; j<numDivs; j++){
             let innerDiv = document.createElement('div');
             innerDiv.className = 'innerDiv';
 
-            gridFormat(innerDiv,scale);
+            innerDiv.style.width=`${containDimension/numDivs}px`;  
+            innerDiv.style.height=`${containDimension/numDivs}px`;              
             
             innerDiv.addEventListener('mouseover',()=>{
                 innerDiv.setAttribute('style','background-color:black;');
-                gridFormat(innerDiv,scale);
+                innerDiv.style.width=`${containDimension/numDivs}px`;  
+                innerDiv.style.height=`${containDimension/numDivs}px`; 
             });
             
             newDiv.appendChild(innerDiv);
@@ -30,13 +38,8 @@ const createDiv = (numDivs) =>{
         sketchDiv.appendChild(newDiv);
     }
 
-
 }
 
-const gridFormat = (div,scale) =>{
-    div.style.padding = `${scale}px `;     
-    div.innerText='_';
-}
 
 
 createDiv(16);
